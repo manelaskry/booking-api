63ff17addcc6734f0c2af48033eb8bc1595d3b8f
manelaskry <manlaskry8@gmail.com>
2024-04-08 15:16:09 +0200

    crud-res

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -59,3 +59,84 @@ function getDatesBetween(startTime, endTime) {
 
     return dates;
 }
+
+
+export const getAllReservations = async (req, res,next) => {
+    try {
+      const reservations = await Reservation.find();
+      res.status(200).json(reservations);
+    } catch (error) {
+      next(err)
+    }
+  };
+
+  // Get a single reservation by ID
+export const getReservationById = async (req, res, next) => {
+    try {
+      const reservation = await Reservation.findById(req.params.id);
+      if (!reservation) {
+        res.status(404).json({ message: "Reservation not found" });
+        return;
+      }
+      res.status(200).json(reservation);
+    } catch (error) {
+      next(err)
+    }
+  };
+
+
+  export const updateReservation = async (req, res, next) => {
+    try {
+      const reservation = await Reservation.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true } //retouner info updated for mongo
+      );
+      if (!reservation) {
+        res.status(404).json({ message: "Reservation not found" });
+        return;
+      }
+      // Extract  info room  from the updated reservation 
+      //const { room } = reservation;
+      // Envoyer un email de confirmation de mise à jour de réservation
+      //const reservationDetails = {
+      //date: reservation.startTime,
+        //room: room 
+      //};
+      //sendReservationModificationEmail('destinataire@example.com', reservationDetails);
+  
+      res.status(200).json(reservation);
+    } catch (error) {
+      next(err)
+    }
+  };
+
+
+  // Delete a reservation by ID
+export const deleteReservation = async (req, res ,next) => {
+    try {
+      const reservation = await Reservation.findByIdAndDelete(req.params.id);
+      if (!reservation) {
+        res.status(404).json({ message: "Reservation not found" });
+        return;
+      }
+      //update the corresponding room to mark it as available
+      const room = await Room.findById(reservation.roomId);
+      if (!room) {
+        res.status(404).json({ message: "Room not found" });
+        return;
+      }
+      const reservationDates = getDatesBetween(reservation.startTime, reservation.endTime);
+        room.unavailableDates = room.unavailableDates.filter(date => !reservationDates.includes(date));
+      await room.save();
+      // Envoyer un email de confirmation de suppression de réservation
+      //const reservationDetails = {
+      //  date: reservation.startTime, 
+       // room: room.name 
+      //};
+      //sendReservationCancellationEmail('destinataire@example.com', reservationDetails);
+      res.status(200).json({ message: "Reservation deleted successfully" });
+    } catch (error) {
+      next(err)
+    }
+  };
